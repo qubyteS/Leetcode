41da2aea90b9b519159df9d32b3a521e4aa117d6
qubyteS <qubytemystery@gmail.com>
2019-09-14 10:00:15 +0530

    3_LongestSubstringWithoutRepeatingChars [HashSet/HashMap][Important][Good]

---
+++ String/LongestSubstringWithoutRepeatingChars.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) 
+	{
+		unordered_set<char> set;
+    
+		int i = 0, j = 0, n = s.size(), ans = 0;
+    
+		while( i<n && j<n) {
+            //If the character does not exist in the set
+			if(set.find(s[j]) == set.end()) {
+				set.insert(s[j++]); //Insert the character in set and update the j counter
+				ans = max(ans, j-i); //Check if the new distance is longer than the current answer
+			}
+			else {
+				set.erase(s[i++]); 
+				/*If character does exist in the set, ie. it is a repeated character, 
+				we update the left side counter i, and continue with the checking for substring. */
+			}
+		}
+    
+		return ans;
+	}
+};
